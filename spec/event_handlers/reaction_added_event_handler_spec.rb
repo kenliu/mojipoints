e4488302953d0dffbb1c0b49c9f389c9366ef9f9
Ken Liu <ken.liu@gmail.com>
2017-02-27 09:33:13 -0500

    update spec

--- spec/event_handlers/reaction_added_event_handler_spec.rb
+++ spec/event_handlers/reaction_added_event_handler_spec.rb
@@ -6,30 +6,58 @@ RSpec.describe ReactionAddedEventHandler do
   let!(:slack_team) { SlackTeam.create(teamid: teamid, bot_userid: bot_userid, bot_oauth_access_token: '123', oauth_access_token: '123')}
   let(:initialize_params) { { team_id: teamid, user: bot_userid } }
   let(:slack_api) { double('slack_api') }
+  let(:original_ts) { '123456.12345' }
+  let(:channel) { 'C123456' }
+  let(:subject_user) { 'U123456' }
+  let(:voter) { 'U111111' }
+  let!(:recognition) { RecognitionsService.create_recognition(teamid, channel, subject_user, '++', 'test reason', voter, original_ts, true) }
+  let(:bot_msg_ts) { '123456.123456' }
+  let(:params) do
+    {
+        event: {
+            item: {
+                ts: bot_msg_ts,
+                channel: channel
+            },
+            channel: 'channel',
+            user: 'UTESTUSER',
+            reaction: '+1'
+        }
+    }
+  end
+
   subject { described_class.new(initialize_params, slack_api) }
 
   it 'ignores self-votes'
 
-  it 'upvotes'
-    let(:original_ts) { '123456.12345' }
-    let(:channel) { 'C123456' }
-    let(:subject_user) { 'U123456' }
-    let(:voter) { 'U111111' }
-    let!(:recognition) { RecognitionsService.create_recognition(teamid, channel, subject_user, '++', 'test reason', voter, original_ts, true) }
-    let(:bot_msg_ts) { '123456.123456' }
-    let(:params) do
-      {
-        event: {
-          item: {
-              ts: bot_msg_ts,
-              channel: channel
-          },
-          channel: 'channel',
-          user: 'UTESTUSER',
-          reaction: '+1'
-        }
-      }
-    end
+  it 'posts reaction on the recognition message' do
+
+    params[:event][:item][:ts] = original_ts
+
+    recognition.bot_msg_ts = bot_msg_ts
+    recognition.save!
+
+    expected = {
+        attachments: [
+            {
+                fallback: "foo has 1 points",
+                title: "foo has 1 points",
+                footer: ":thumbsup: by <@user>"
+            }
+        ],
+        channel: 'channel'
+    }
+    expect(Recognition.count).to eq 1
+    expect(recognition.votes.size).to eq 1 # precondition
+
+    expect(slack_api).to receive(:chat_update)
+
+    subject.handle(params)
+
+    recognition = Recognition.first
+    expect(recognition.votes.size).to eq 2
+    expect(recognition.votes.last.first_vote).to be_falsey
+  end
 
   it 'upvotes a thing with no reason' do
     recognition.bot_msg_ts = bot_msg_ts
