b63844a451466486833f9ff73ffc01b769cdaeca
Ken Liu <ken.liu@gmail.com>
2017-02-27 21:40:55 -0500

    include scout instrumentation needed for API controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::API
+  # include scout instrumentation needed for API controllers
+  include ScoutApm::Instruments::ActionControllerRails3Rails4Instruments
 end
