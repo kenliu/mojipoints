34e7942601bb1ab7ac211b86ae13e66627bb0949
Ken Liu <ken.liu@gmail.com>
2017-03-07 09:02:02 -0500

    use a better method to find a DM channel

--- app/services/slack_messaging_service.rb
+++ app/services/slack_messaging_service.rb
@@ -1,7 +1,11 @@
 class SlackMessagingService
-
   def self.im_channel(teamid, userid)
+    # performance note: in the future we might want to
+    # cache the user's DM channel on the SlackUser object
+    # to avoid the overhead of hitting this API
+    # for every single DM
     api = SlackApiService.create_slack_client(teamid)
-    api.im_list['ims'].find { |im| im['user'] == userid }['id']
+    response = api.im_open(user: userid)
+    response[:channel][:id]
   end
 end
